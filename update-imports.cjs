a4a4e65adbc7a7a89bded38728992f8e5bdf445c
rodrigomd94 <51804048+rodrigomd94@users.noreply.github.com>
2023-09-25 12:04:03 -0600

    Test fixes

--- update-imports.cjs
+++ update-imports.cjs
@@ -1,11 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 
+function fileExists(filePath) {
+  try {
+    return fs.statSync(filePath).isFile();
+  } catch (err) {
+    return false;
+  }
+}
+
 function updateImports(dir) {
   const files = fs.readdirSync(dir);
 
   for (const file of files) {
     const filePath = path.join(dir, file);
+
+    // Skip over lucid-cardano related files or directories
+    if (filePath.includes('lucid-cardano')) {
+      continue;
+    }
 
     if (fs.statSync(filePath).isDirectory()) {
       updateImports(filePath);
@@ -14,8 +27,12 @@ function updateImports(dir) {
 
       // Regex to find import statements and append .js
       content = content.replace(/from ['"]([^'"]+)['"]/g, (match, p1) => {
-        if (!p1.endsWith('.js') && !p1.startsWith('http') && !p1.includes('@') && !p1.includes('lucid-cardano')) {
-          return `from '${p1}.js'`;
+        if (!p1.endsWith('.js') && !p1.startsWith('http') && !p1.includes('@')) {
+          const possibleLocalPath = path.join(dir, `${p1}.js`);
+          const possibleNodeModulePath = path.join(process.cwd(), 'node_modules', `${p1}.js`);
+          if (fileExists(possibleLocalPath) || fileExists(possibleNodeModulePath)) {
+            return `from '${p1}.js'`;
+          }
         }
         return match;
       });
@@ -24,7 +41,6 @@ function updateImports(dir) {
     }
   }
 }
-
 
 // Starting directory
 updateImports('./build');
